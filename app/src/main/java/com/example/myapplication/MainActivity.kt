5958fc2963cd854f2709447ab0c6d48baa891c0f
Alexey <schneiderff37@gmail.com>
2022-07-24 20:29:34 +0500

    init3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,29 +10,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val name = ("Maksonchik")
         Log.d("MyLog", "Name:${name}")
-        val numbers = arrayOf(1 to 15)
-        for (i in 1..15)
+        val numbers = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+        for (i in numbers)
             println(i)
-        println(numbers)
-        
-        println("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15")
+        for (i in numbers) {
+            if (i % 2 != 0) println(i)
+        }
+        for (i in numbers) {
+            if (i % 2 == 0) println(i)
+        }
+
         println("Max")
         val numberss = arrayOf(2,7,3,6,9,14,8,1)
         numberss.sort()
         println(numberss)
+
         val womanname = arrayOf("Lola, Huela, Vika, Slava, Izolda")
-        Log.d("MyLog3","Names:${womanname[0]}")
-        Log.d("MyLog3","Names:${womanname[1]}")
-        Log.d("MyLog3","Names:${womanname[2]}")
-        Log.d("MyLog3","Names:${womanname[3]}")
-        Log.d("MyLog3","Names:${womanname[4]}")
+        for (i in womanname)
+        println(i)
 
         val mass5 = arrayOf(1,2,3,4,5)
-        Log.d("MyLog2", "Numbers: ${mass5[0]}${mass5[0]}${mass5[0]}${mass5[0]}${mass5[0]}")
-        Log.d("MyLog2", "Numbers: ${mass5[1]}${mass5[1]}${mass5[1]}${mass5[1]}${mass5[1]}")
-        Log.d("MyLog2", "Numbers: ${mass5[2]}${mass5[2]}${mass5[2]}${mass5[2]}${mass5[2]}")
-        Log.d("MyLog2", "Numbers: ${mass5[3]}${mass5[3]}${mass5[3]}${mass5[3]}${mass5[3]}")
-        Log.d("MyLog2", "Numbers: ${mass5[4]}${mass5[4]}${mass5[4]}${mass5[4]}${mass5[4]}")
+        for (i in mass5)
+        repeat(5) {
+            println(i)
+        }
 
         val massivB= arrayOf(1, 8, 2, 4, 8, 4, 3, 5)
         massivB.sortDescending()
